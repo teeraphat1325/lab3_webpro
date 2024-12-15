ab6e809aecbdac8128c81e13617220e1bb9cac3a
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-15 21:31:28 +0700

    Add html in TodoListView.vue

--- lab3-project/src/views/TodoListView.vue
+++ lab3-project/src/views/TodoListView.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>TodoListView</div>
+    <div>
+        <h1>Todo List</h1>
+        <input type="text"><button>Add</button>
+        <ul>
+            <li><span>abc</span><button>Remove</button></li>
+            <li><span class="completed">def</span><button>Remove</button></li>
+            <li><span>ghi</span><button>Remove</button></li>
+        </ul>
+    </div>
 </template>
 
 <script setyp lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.completed {
+    text-decoration: line-through;
+    color: gray;
+}
+button {
+    margin-left:10px ;
+}
+</style>
