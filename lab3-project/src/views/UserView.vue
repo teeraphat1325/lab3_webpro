9dde814b23e6d89aae9ea280962cb9452393d2ab
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-17 02:29:11 +0700

    Add addUser CRUD User

--- lab3-project/src/views/UserView.vue
+++ lab3-project/src/views/UserView.vue
@@ -2,9 +2,9 @@
     <div id="page">
         <h1>User Management</h1>
         <div>
-            <input type="text" placeholder="Enter email">
-            <input type="password" placeholder="Enter password">
-            <button>Save</button>
+            <input type="text" placeholder="Enter email" v-model="form.email">
+            <input type="password" placeholder="Enter password" v-model="form.password">
+            <button @click="addUser">Save</button>
             <button>Cancel</button>
         </div>
         <div>
@@ -18,25 +18,40 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>email1@mail.com</td>
-                        <td>password</td>
+                    <tr v-for="user in users" :key="user.id">
+                        <td>{{ user.id }}</td>
+                        <td>{{ user.email }}</td>
+                        <td>{{ user.password }}</td>
                         <td><button>Edit</button><button>Delete</button></td>
                     </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>email2@mail.com</td>
-                        <td>password</td>
-                        <td><button>Edit</button><button>Delete</button></td>
-                    </tr>
+
                 </tbody>
             </table>
         </div>
     </div>
 </template>
 
-<script setup lang='ts'></script>
+<script setup lang='ts'>
+import { ref, reactive } from 'vue'
+interface User {
+    id: number
+    email: string
+    password: string
+}
+const form = ref<User>({ id: 0, email: '', password: '' })
+const users = ref<User[]>([])
+const lastId = ref(1)
+const editedId = ref<number | null>(null)
+function addUser() {
+    users.value.push({ ...form.value, id: lastId.value })
+    lastId.value++
+    clearform()
+}
+function clearform() {
+    form.value = { id: 0, email: '', password: '' }
+}
+
+</script>
 
 <style scoped>
 #page {
